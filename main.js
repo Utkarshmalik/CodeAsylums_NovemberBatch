abe6cb40abfc7ff35b0220e1f242eda1b51632d8
mohitverma6161 <utkarshmalik06@gmail.com>
2020-12-02 22:45:50 +0530

    execution context

---
+++ main.js
@@ -0,0 +1,60 @@
+
+
+
+/*
+what is execution context in JS ? 
+*/
+
+
+/* how is this JS code executed ?  */
+
+// var x=10;
+
+// function parent()
+// {
+//     var y=10;
+
+//     console.log("In parent");
+    
+//     function child() {
+//         console.log("In Child")   
+//     }
+
+//     child();
+// }
+
+// parent();
+
+function fun(params) {
+    var y=15;
+    console.log(x);
+}
+
+console.log(y);
+var x=10;
+
+fun();
+
+
+
+/*
+
+Scope of x 
+*/
+
+
+/*
+Execution context:
+
+Global Execution Context
+Functional Execution Context 
+
+*/
+
+
+/*
+
+scope:
+Lexical environment :
+
+*/
