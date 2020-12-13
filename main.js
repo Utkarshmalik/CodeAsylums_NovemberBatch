e653316b795815d5df131489a3251070a5b95915
mohitverma6161 <utkarshmalik06@gmail.com>
2020-12-13 22:37:49 +0530

    this keyword exercises

--- main.js
+++ main.js
@@ -1,158 +1,30 @@
-// // function a(params) {
+// function logThis() 
+// { console.log(this);
+// }
+
+// const myObj = { name: "sag1v" }
+
+// logThis.apply(myObj)
+
+// class myClass { 
   
-// //   var b=10;
-
-// //   function c(params) {
-// //     console.log(b);
-// //   }
-
-// //   return c;
-// // }
-
-// // let anotherFun=a();
-// // anotherFun();
-
-// function  grandparent(params) {
-
-//   var x=5;
-
-//   function parent(params) {
-
-//     var y=6;
-
-//     function child(params) {
-
-//       var z=7;
-//       console.log(x+y+z);
-//     }
-    
-//     return child;
-//   }
-
-//   return parent;
+//   logThis = () =a {
+//   console.log(this); 
+// }
 // }
 
- 
-// var parent=grandparent();
-// var childd=parent();
-// childd();
+//   const myObj = { name: 'sagiv' };
+
+//   const myClassInstance = new myClass()
+
+//    myClassInstance.logThis.call(myObj)
 
 
-// //CLOSURES
+const call = {
+  caller: "mom", anotherCaller: function() {
+  console.log(`${this.caller} called, too!`) },
+  says: function() {
+  console.log(`Hey, ${this.caller} just called.`);
+  } };
 
-
-
-// function fun(params) {
-//   console.log(this);
-// }
-
-// fun();
-
-// function fun(params) {
-//   console.log(this);
-// }
-
-// const obj={
-//    print:fun
-// }
-
-// obj.print();
-
-
-
-
-//this points to the object to which the function that is running belongs to 
-
-
-// function  grandparent(params) {
-
-//   function parent(params) {
-//     console.log(this);
-//   }
-
-//   return parent;
-// }
-
-// const parent=grandparent();
-// parent();
-
-
-// const obj1={
-  
-//   foo:function foo(params) {
-
-//     console.log(this);
-
-//     function bar(params) {
-//       console.log(this);
-//     }
-//     return bar;
-//   }
-// }
-
-// obj1.foo()();
-
-
-// const bar=obj1.foo();
-
-// bar();
-
-
-//arrow functions
-
-
-//this inside of arrow functions will be whatever it was set by the wrapping executiion context 
-
-
-// const fun1=function (params) {
-//   console.log(this);
-// }
-
-// const fun=()=>
-// {
-//   console.log(this);
-// }
-
-// const obj={name:fun};
-
-
-// obj.name();
-
-
-// function fun(params) {
-//   console.log(this);
-// }
-
-// const someObj=new fun();
-
-// console.log(someObj);
-
-//call 
-
-// const obj1={
-//   name:"Utkarsh"
-// }
-
-// const obj2={
-//   name:"Rahul",
-//   attack:function attack(weapon,time) {
-//     console.log(this.name + " is attacking with "+weapon+" at "+time);
-//   }
-// }
-
-// //obj2.attack("gun","night");
-
-// // obj2.attack.apply(obj1,["rifle","mid night"]);
-
-// //bind
-
-// const fun=obj2.attack.bind(obj1,"rifle","midnight");
-
-// fun();
-
-function fun(params) {
-  console.log(this);
-}
-
-const btn = document.getElementById('btn');
-btn.addEventListener('click',fun);
+  let newCall = call.anotherCaller; newCall();
