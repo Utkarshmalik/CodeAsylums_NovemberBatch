860db3adeeb5d67ff19b14c6d321c3eb438828fc
mohitverma6161 <utkarshmalik06@gmail.com>
2020-12-16 22:26:20 +0530

    shallow copy vs deep copy

--- main.js
+++ main.js
@@ -1,30 +1,101 @@
-// function logThis() 
-// { console.log(this);
+
+
+
+
+// for(let i=0;i<arr.length;i++)
+// {
+//   console.log(arr[i].name+" is"+arr[i].age+" years old");
 // }
 
-// const myObj = { name: "sag1v" }
+// //forEach
+// arr.forEach((item,index)=>{
+//   arr[index].name=item.name+" jain";
+// });
 
-// logThis.apply(myObj)
+// console.log(arr);
 
-// class myClass { 
-  
-//   logThis = () =a {
-//   console.log(this); 
+
+//filter
+
+// const modifiedArray=arr.filter((item)=>
+// {
+//   return item.age > 25; 
+// });
+
+
+// console.log(arr);
+// console.log(modifiedArray);
+
+
+//map
+
+// arr.map((item)=>
+// {
+//   return item.age+30
+// });
+
+//shallow copy : only the memory addresses are copied
+
+// var arr1=[1,2,3,4,5,6,7,8];
+// var arr2=arr1;
+
+// arr2.push(9);
+
+// console.log(arr1);
+// console.log(arr2);
+
+
+
+// const arr2=arr1.map((element)=>
+// {
+//   return element+5;
+// });
+
+// console.log(arr2);
+
+
+
+// var arr={name:"utkarsh",age:26,height:185};
+
+// //spread operator 
+// var arr1={...arr};
+
+// arr1.age=30;
+
+// console.log(arr);
+// console.log(arr1);
+
+// var arr1=[1,2,3,4,5,6];
+// var arr2=[...arr1];
+
+// arr2.push(7);
+// console.log(arr1);
+// console.log(arr2);
+
+
+
+// var arr={ details:{name:"utkarsh",age:26,height:185},contacts:["rahul","rakhi"]};
+
+// // //spread operator 
+// var arr1={...arr};
+
+// arr1.details={...arr.details};
+// arr1.contacts=[...arr.contacts];
+
+// arr1.details.name="aakarsh";
+
+// console.log(arr);
+// console.log(arr1);
+
+
+// var arr=[{name:"Utkarsh",age:26},{name:"Rahul",age:22},{name:"Shreya",age:30}];
+
+// var arr1=[...arr];
+
+// for(let i=0;i<arr.length;i++)
+// {
+//   arr1[i]={...arr[i]}
 // }
-// }
 
-//   const myObj = { name: 'sagiv' };
-
-//   const myClassInstance = new myClass()
-
-//    myClassInstance.logThis.call(myObj)
-
-
-const call = {
-  caller: "mom", anotherCaller: function() {
-  console.log(`${this.caller} called, too!`) },
-  says: function() {
-  console.log(`Hey, ${this.caller} just called.`);
-  } };
-
-  let newCall = call.anotherCaller; newCall();
+const arr=[];
+console.log(arr);
