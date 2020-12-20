b35034cee336c38ef37306e51db4fab3e70992b7
mohitverma6161 <utkarshmalik06@gmail.com>
2020-12-20 22:43:14 +0530

    DOM Manipulation project TODO lis

--- main.js
+++ main.js
@@ -1,71 +1,42 @@
 
+const addButton=document.getElementById("addButton");
 
-// //innerHTML
+addButton.addEventListener("click",()=>
+{
+    var li=document.createElement("li");
 
-// const div=document.getElementById("head");
-
-// // console.log(div.hasAttribute('class'))
-
-// const arr=["HTML","CSS","JS","React","NodeJS"];
-
-// /*
-// createElement()
-// */
+    var inputFieldValue=document.getElementById("inputField").value;
 
 
-// /*
-// getAttribute,set,has,remove 
-// */
+    if(inputFieldValue!=="")
+    {
+    var textNode=document.createTextNode(inputFieldValue);
+    li.appendChild(textNode);
+    li.style.margin="5px";
+    var span=document.createElement('SPAN');
+    span.setAttribute("class","deleteButton");
+    var txtNode=document.createTextNode('X');
+    span.appendChild(txtNode);
+    span.style.float="right";
+    li.appendChild(span);
+    document.getElementById('taskList').appendChild(li);   
+    } 
+    document.getElementById("inputField").value="";
 
-// const list=document.createElement('ul');
+    li.onclick=(event)=>
+    {
+        li.setAttribute("class","checked");
+    }
 
+    span.onclick=(event)=>
+    {
+        event.stopPropagation();
+        li.style.display="none";
+    }
 
-// arr.forEach((data)=>
-// {
-//     const li=document.createElement('li');
-//     li.innerHTML=data;
-//     li.setAttribute("class","listItem");
+})
 
-//     setTimeout(() => {
-//         li.removeAttribute('class');
-//     }, 3000);
-
-//     /*
-//     appendChild
-//     */
-//     list.appendChild(li);
-// });
-
-// div.appendChild(list);
-
-// const para=document.createElement('p');
-
-// para.innerHTML='hello how are you';
-
-// div.appendChild(para);
-
-const arr=[1,2,3,4,5,6,7,8];
-
-
-function reducer(prevValue,num) {
-    return prevValue+num;
-
-}
-
-const initValue=0;
-
-const ans=arr.reduce(reducer,initValue);
-
-// prevValue,num
-// 0,1
-// 1,2
-// 3,3
-// 6,4
-// 10,5
-// 15,6
-// 21,7
-// 28,8
-// 36
-
-
-console.log(ans);
+document.getElementById("clearButton").addEventListener("click",()=>
+{
+    document.getElementById("taskList").innerHTML="";
+})
