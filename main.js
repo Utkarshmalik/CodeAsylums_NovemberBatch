e298f6bc6fb5a995dd88f49260d65a49baf51510
mohitverma6161 <utkarshmalik06@gmail.com>
2020-12-27 16:51:50 +0530

    debouncing

--- main.js
+++ main.js
@@ -1,27 +1,52 @@
 
 
-const btn=document.getElementById("btn");
+// const btn=document.getElementById("btn");
 
-// request should be made only once in 2 sec 
+// // request should be made only once in 2 sec 
 
-var timer=undefined;
+// var timer=undefined;
 
-btn .addEventListener("click",()=>{
+// btn .addEventListener("click",()=>{
 
-    if(timer)
-    {
-        return;
-    }
+//     if(timer)
+//     {
+//         return;
+//     }
 
 
-    fetch("https://dummyapi.io/data/api/user",{
-        headers:{"app-id":"5fca5a3236a334a6a8f40cc7"}
-    }).then(data=>console.log(data));
+//     fetch("https://dummyapi.io/data/api/user",{
+//         headers:{"app-id":"5fca5a3236a334a6a8f40cc7"}
+//     }).then(data=>console.log(data));
 
+//     timer=setTimeout(() => {
+//         timer=undefined;
+//     }, 3000);
+    
+// })
+
+
+
+
+
+
+
+
+var inputField=document.getElementById('search');
+
+
+var timer;
+
+inputField.addEventListener("input",(e)=>
+{
+
+    clearTimeout(timer);
+    
     timer=setTimeout(() => {
-        timer=undefined;
-    }, 3000);
+        
+        fetch("https://dummyapi.io/data/api/user",{
+                    headers:{"app-id":"5fca5a3236a334a6a8f40cc7"}
+                }).then(data=>console.log(data));
+        
+    }, 2000);
     
 })
-
-
