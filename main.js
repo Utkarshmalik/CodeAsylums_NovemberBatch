9c5d16ba047ce5912df9c1aa164531af63ca3b43
mohitverma6161 <utkarshmalik06@gmail.com>
2020-12-27 17:10:13 +0530

    cors

--- main.js
+++ main.js
@@ -1,52 +1,8 @@
+//cross origin resource policy
 
 
-// const btn=document.getElementById("btn");
+//same origing 
 
-// // request should be made only once in 2 sec 
+https://www.example.com/foobar.html
 
-// var timer=undefined;
-
-// btn .addEventListener("click",()=>{
-
-//     if(timer)
-//     {
-//         return;
-//     }
-
-
-//     fetch("https://dummyapi.io/data/api/user",{
-//         headers:{"app-id":"5fca5a3236a334a6a8f40cc7"}
-//     }).then(data=>console.log(data));
-
-//     timer=setTimeout(() => {
-//         timer=undefined;
-//     }, 3000);
-    
-// })
-
-
-
-
-
-
-
-
-var inputField=document.getElementById('search');
-
-
-var timer;
-
-inputField.addEventListener("input",(e)=>
-{
-
-    clearTimeout(timer);
-    
-    timer=setTimeout(() => {
-        
-        fetch("https://dummyapi.io/data/api/user",{
-                    headers:{"app-id":"5fca5a3236a334a6a8f40cc7"}
-                }).then(data=>console.log(data));
-        
-    }, 2000);
-    
-})
+http://www.en.example.com/helloworld.html
